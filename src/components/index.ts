bb3ed2518ade815b0cdd72648001ecb9818b0148
Mohamed Abdelaziz <amrikyy@gmail.com>
2026-07-26 06:24:12 +0300

    fix: Header is default export, not named export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 // Core UI Primitives
 export { GradientText, ShimmerText, Counter, MagneticButton, FloatingActionButton, ParticleBackground, Typewriter, PAILogo } from './pai/ui';
 export { TiltCard } from './effects';
-export { Header } from './Header';
+export { default as Header } from './Header';
