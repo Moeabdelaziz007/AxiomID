41b88f4aae1072d888ce1c2bea6b3cd4dd7da118
Mohamed Abdelaziz <amrikyy@gmail.com>
2026-07-26 09:07:27 +0300

    fix: pai/index.ts - correct import paths (./ui and ../../effects)

--- src/components/pai/index.ts
+++ src/components/pai/index.ts
@@ -1,4 +1,4 @@
 // Core UI Primitives
-export { GradientText, ShimmerText, Counter, MagneticButton, FloatingActionButton, ParticleBackground, Typewriter, PAILogo } from '../ui';
+export { GradientText, ShimmerText, Counter, MagneticButton, FloatingActionButton, ParticleBackground, Typewriter, PAILogo } from './ui';
 export { TiltCard } from '../../effects';
 export { default as Header } from './Header';
